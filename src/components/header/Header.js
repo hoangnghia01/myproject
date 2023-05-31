1b9a025af48f321226b2f165edd28d21f11803c7
hoangnghia01 <130352626+hoangnghia01@users.noreply.github.com>
2023-06-01 04:00:49 +0700

    update 1-5

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,12 +5,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import { BsCart3 } from 'react-icons/bs';
 import { CgMenuRight } from 'react-icons/cg';
+import { GrFormDown } from 'react-icons/gr';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BsMinecart } from 'react-icons/bs';
+
 import logo from "../logo/logo.jpg"
 import "./Header.css"
 export default function Header() {
     return (
 
-        <Navbar expand="lg">
+        <Navbar expand="lg" className='navbar'>
             <Container>
                 <div className='header_left'>
                     <Link to={"/"} style={{ textDecoration: 'none' }} ><img src={logo} /></Link>
@@ -20,31 +24,61 @@ export default function Header() {
                     {/* <CgMenuRight className='menu_icon'/> */}
 
 
-
-
-
-
-                </div>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <div className='menu'>
-                        <Link to={"/"} style={{ textDecoration: 'none' }}><h3>Home</h3></Link>
-                        <Link to={"/giai-phap"} style={{ textDecoration: 'none' }} className='giaiphap '><h3>Giai phap</h3></Link>
-                        <div className='menu_giaiphap'>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <div className='menu'>
                             <ul>
-                                <li>Giai phap 1</li>
-                                <li>Giai phap 1</li>
-                                <li>Giai phap 1</li>
-                                <li>Giai phap 1</li>
-                                <li>Giai phap 1</li>
+                                <li><Link to={"/"} style={{ textDecoration: 'none' }}><h3>Home</h3></Link></li>
+                                <li><Link to={"/giai-phap"} style={{ textDecoration: 'none' }} className='solusion'><h3>Giai phap <span><GrFormDown /></span></h3>
+
+                                    <ul className='menu_child'>
+                                        <li>Giai phap 1
+                                            <ul className='menu_child-child'>
+                                                <li>ggggggggg1</li>
+                                                <li>ggg1</li>
+                                                <li>ggg1</li>
+                                            </ul>
+                                        </li>
+                                        <li>Giai phap 1</li>
+                                        <li>Giai phap 1</li>
+                                        <li>Giai phap 1</li>
+                                    </ul>
+
+                                </Link></li>
+
+                                <li><Link to={"/san-pham"} style={{ textDecoration: 'none' }} className='product'>
+                                    <h3>San pham <span><GrFormDown /></span></h3>
+
+                                    <ul className='menu_child'>
+                                        <li>San pham 1</li>
+                                        <li>San pham 2 1</li>
+                                        <li>San pham 3 1</li>
+                                        <li>San pham 4</li>
+                                    </ul>
+
+                                </Link></li>
+
+                                <li><Link to={"/lien-he"} style={{ textDecoration: 'none' }}><h3>Lien he</h3></Link></li>
                             </ul>
                         </div>
-                        <Link to={"/san-pham"} style={{ textDecoration: 'none' }}><h3>San pham</h3></Link>
+                    </Navbar.Collapse>
+                    <div className='search'>
+                        <input placeholder='Search' />
+                        <button className='icon_seach'><AiOutlineSearch /></button>
 
-                        <Link to={"/lien-he"} style={{ textDecoration: 'none' }}><h3>Lien he</h3></Link>
                     </div>
-                </Navbar.Collapse>
-
-                <BsCart3 className='cart' />
+                    <div className='shopping_cart'>
+                        <BsCart3 className='cart_icon' />
+                        <p className='cart_item'>0</p>
+                        <div className='container_cart'>
+                            <div className='product_in_cart'><BsMinecart/></div>
+                            <div className='total'>
+                                <h5>Tong cong:</h5>
+                                <h5>$0</h5>
+                            </div>
+                            <button><h5>Gio hang</h5></button>
+                        </div>
+                    </div>
+                </div>
             </Container>
         </Navbar>
 
