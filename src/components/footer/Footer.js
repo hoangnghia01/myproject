d3886f7b02ed23faa515c1476cb5ed9df4086826
EV <tnt963852741321@gmail.com>
2023-06-01 21:12:26 +0700

    01-06

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,50 @@
+import { Container } from "react-bootstrap"
 import "./Footer.css"
-export default function Footer(){
-    return(
-        <div>footer</div>
+import logo from "../logo/logo.jpg"
+import { Link } from "react-router-dom"
+
+import { AiOutlineRight } from 'react-icons/ai';
+export default function Footer() {
+    return (
+        <div className="footer">
+            <Container>
+                <div className="footer_top">
+                    <div className="footer_left">
+                        <img src={logo}/>
+                        <p>Morbi convallis bibendum urna ut viverra. Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper.</p>
+                    </div>
+                    <div className="helpful"><h4>Helpful</h4>
+                        <ul>
+                            <li><AiOutlineRight/>  <Link to={"/"}>About Us</Link></li>
+                            <li><AiOutlineRight/>  <Link to={"/"}>About Us</Link></li>
+                            <li><AiOutlineRight/>  <Link to={"/"}>About Us</Link></li>
+                            <li><AiOutlineRight/>  <Link to={"/"}>About Us</Link></li>
+                        </ul>
+                    </div>
+                    <div className="support"><h4>Support</h4>
+                        <ul>
+                            <li><AiOutlineRight/>  <Link to={"/"}>Contact</Link></li>
+                            <li><AiOutlineRight/>  <Link to={"/"}>Contact</Link></li>
+                            <li><AiOutlineRight/>  <Link to={"/"}>Contact</Link></li>
+                            <li><AiOutlineRight/>  <Link to={"/"}>Contact</Link></li>
+                        </ul>
+                    </div>
+               
+                <div className="contact_us"><h4>Contact Us</h4>
+                    <ul>
+                        <li><p>455 West Orchard Street
+                            Kings Mountain, NC 28086</p></li>
+                        <li><p>Phone: (272) 211-7370</p></li>
+                        <li><p>Email: (272) 211-7370</p></li>
+                        <li></li>
+                    </ul>
+                </div>
+                </div>
+                <div className="footer_bot" >
+                    <p>© 2023 Turing. All Rights Reserved.</p>
+                </div>
+            </Container>
+        </div>
+
     )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -20,8 +20,8 @@ export default function Header() {
                     <Link to={"/"} style={{ textDecoration: 'none' }} ><img src={logo} /></Link>
                 </div>
                 <div className='header_right'>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" className='icon_menu' />
-                    {/* <CgMenuRight className='menu_icon'/> */}
+                    
+                    
 
 
                     <Navbar.Collapse id="basic-navbar-nav">
@@ -61,22 +61,35 @@ export default function Header() {
                             </ul>
                         </div>
                     </Navbar.Collapse>
+                    <CgMenuRight className='menu_icon'/>
                     <div className='search'>
-                        <input placeholder='Search' />
-                        <button className='icon_seach'><AiOutlineSearch /></button>
-
+                        
+                        <button className='icon_search'><AiOutlineSearch /></button>
+                       
+                        
+                        <div className='container_search'>
+                           <Container>
+                            Nghix
+                           </Container>
+                        </div>
+                        
                     </div>
                     <div className='shopping_cart'>
+
                         <BsCart3 className='cart_icon' />
-                        <p className='cart_item'>0</p>
                         <div className='container_cart'>
-                            <div className='product_in_cart'><BsMinecart/></div>
+                            <div className='product_in_cart'><BsMinecart /></div>
                             <div className='total'>
                                 <h5>Tong cong:</h5>
                                 <h5>$0</h5>
                             </div>
                             <button><h5>Gio hang</h5></button>
                         </div>
+
+
+
+                        <p className='cart_item'>0</p>
+
                     </div>
                 </div>
             </Container>
